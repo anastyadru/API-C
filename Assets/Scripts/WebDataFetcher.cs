30becc9526ecb69318175f3e406458cec4f95376
Nastia <adruts@mail.ru>
2023-12-08 19:58:52 +0300

    https://github.com/anastyadru/API-C.git

---
+++ Assets/Scripts/WebDataFetcher.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine.UI;
+using System.Xml;
+
+
+public class WebDataFetcher : MonoBehaviour
+{
+    public string url;
+    public Text listItemPrefab;
+    public Transform contentTransform;
+
+    private async void Start()
+    {
+        
+    }
+}
